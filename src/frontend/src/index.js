8bbafdfd34ef3a9f2e30dd794bc24523c5ef9124
Otto Lima <otto.lima@sou.inteli.edu.br>
2024-04-06 19:17:07 -0300

    commit 1

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Importar páginas
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const rootElement = document.getElementById('root');
+
+const renderApp = () => {
+  createRoot(rootElement).render(
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ConnectPage/>} />
+          <Route path="/visualizar-cotação" element={<CotationPage/>} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
+  );
+};
+
+renderApp();
+
 reportWebVitals();
